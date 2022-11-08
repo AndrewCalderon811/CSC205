7470177d632f079a2056ee98bbb5b7b745e84022
Andrew <111481804+AndrewCalderon811@users.noreply.github.com>
2022-11-08 09:59:41 -0800

    Setting up project8

--- CSC/src/jsjf/exceptions/EmptyCollectionException.java
+++ CSC/src/jsjf/exceptions/EmptyCollectionException.java
@@ -12,8 +12,8 @@ public class EmptyCollectionException extends RuntimeException
 	 * Sets up this exception with an appropriate message.
 	 * @param collection the name of the collection
 	 */
-	public EmptyCollectionException(String collection)
+	public EmptyCollectionException (String collection)
 	{
-		super("The " + collection + " is empty.");
+		super ("The " + collection + " is empty.");
 	}
 }
