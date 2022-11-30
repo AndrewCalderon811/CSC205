8a6b5a6eaa538d1cba5f6a1dc766039de11da804
Andrew <111481804+AndrewCalderon811@users.noreply.github.com>
2022-11-30 12:18:29 -0700

    Finished all of project 9

--- CSC/src/project9/HeapDriver.java
+++ CSC/src/project9/HeapDriver.java
@@ -1,5 +1,38 @@
 package project9;
 
+import jsjf.ArrayHeap;
+import jsjf.HeapQueue;
+import jsjf.HeapStack;
+
 public class HeapDriver {
 
+	public static void main(String[]args) {
+		
+		String sentance[] = {"This", "is", "my", "heaps", "project", "for", "CSC205"};
+		ArrayHeap<String> arrayHeap = new ArrayHeap<String>();
+		HeapStack<String> stackHeap = new HeapStack<String>();
+		HeapQueue<String> queueHeap = new HeapQueue<String>();
+		
+		System.out.println(Sorting.smallest_n(sentance, 4));
+		
+		for(String s : sentance) {
+			stackHeap.push(s);
+			queueHeap.enqueue(s);
+		}
+		
+		System.out.println("\nBegin Stack testing: \n");
+		
+		while(!stackHeap.isEmpty()) {
+			System.out.println(stackHeap.pop());
+		}
+		
+		System.out.println("\n--Finished testing Stack-- \nBegin Queue Testing: \n");
+		
+		while(!queueHeap.isEmpty()) {
+			System.out.println(queueHeap.dequeue());
+		}
+		
+	}
+	
+	
 }
